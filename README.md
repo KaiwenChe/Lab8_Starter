c7f8f2e2ae1adc80ca5531881f49f1346627757d
Kaiwen Che <kche@ucsd.edu>
2022-11-21 00:52:30 -0800

    finished explore

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab 8 - Starter
 1. Within a Github action that runs whenever code is pushed. This not only would save me time testing manually locally, but also ensure that every progress made of the code is being checked continuously throughout the development. 
 2. No.
+3. No. Unit tests can't test how these individual components interact with each other on an application/feature level. Since messaging between users invoves mutiple functions and features inside an app, testing with unit test would be time consuming and inefficient.
+4. Yes. This is a relatively samll scale feature that could be tested easily and quickly. Changing other features also won't likely affect the unit tests for this feature.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('functionality of isPhoneNumber', () => {
+    expect(functions.isPhoneNumber('858-539-6950')).toBe(true);
+});
+
+test('functionality of isPhoneNumber', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('functionality of isPhoneNumber', () => {
+    expect(functions.isPhoneNumber('this-is-a-phone-number')).toBe(false);
+});
+
+test('functionality of isPhoneNumber', () => {
+    expect(functions.isPhoneNumber('#1234567890')).toBe(false);
+});
+
+test('functionality of isEmail', () => {
+    expect(functions.isEmail('Rancoon@ucsd.edu')).toBe(true);
+});
+
+test('functionality of isEmail', () => {
+    expect(functions.isEmail('khosla@gmail.com')).toBe(true);
+});
+
+test('functionality of isEmail', () => {
+    expect(functions.isEmail('114514')).toBe(false);
+});
+
+test('functionality of isEmail', () => {
+    expect(functions.isEmail('s1mplegggggggggggiiiiiiiiigggaccchhhhadddddducla.edu')).toBe(false);
+});
+
+test('functionality of isStrongPassword', () => {
+    expect(functions.isStrongPassword('password')).toBe(true);
+});
+
+test('functionality of isStrongPassword', () => {
+    expect(functions.isStrongPassword('strongPassWord')).toBe(true);
+});
+
+test('functionality of isStrongPassword', () => {
+    expect(functions.isStrongPassword('1145141919810')).toBe(false);
+});
+
+test('functionality of isStrongPassword', () => {
+    expect(functions.isStrongPassword('OwO')).toBe(false);
+});
+
+test('functionality of isDate', () => {
+    expect(functions.isDate('01/01/2001')).toBe(true);
+});
+
+test('functionality of isDate', () => {
+    expect(functions.isDate('07/18/2077')).toBe(true);
+});
+
+test('functionality of isDate', () => {
+    expect(functions.isDate('nintyeightyfour')).toBe(false);
+});
+
+test('functionality of isDate', () => {
+    expect(functions.isDate('1919/08/10')).toBe(false);
+});
+
+test('functionality of isHexColor', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+
+test('functionality of isHexColor', () => {
+    expect(functions.isHexColor('#000000')).toBe(true);
+});
+
+test('functionality of isHexColor', () => {
+    expect(functions.isHexColor('red')).toBe(false);
+});
+
+test('functionality of isHexColor', () => {
+    expect(functions.isHexColor('255,255,255')).toBe(false);
+});
